328b3061b8c7b5226eb642b8c1cbd7071a2c00a8
102660231 <48194706+102660231@users.noreply.github.com>
2019-04-18 17:29:55 +1000

    new guess

---
+++ guessgame.cs/guessgame.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace guessgame.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int[] userInput = new int[7];
+            int counter = 1;
+            int answer = 7;
+
+            while (counter <= userInput.Length)
+            {
+                Console.WriteLine("Hey there! Number guesing game ");
+                Console.Write("Please enter a number);
+
+
+
+
+
+            }
+        }
+    }
+}
